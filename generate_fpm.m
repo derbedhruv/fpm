4b085f47902286189619c9593f702fe19900da09
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-05-10 17:09:45 +0530

    have successfully implemented the illumination matrix, or at least looks like it, also removed spiral.m since its not needed

--- generate_fpm.m
+++ generate_fpm.m
@@ -10,5 +10,28 @@ object = intensity_image.*exp(j*phase_image);
 
 %% Illuminate the object at different angles, save images formed
 %% we choose the origin to be the center of the image, for ease of calculation of the illumination wavevector
-%% The light source is assumed to be an LED matrix centered at origin - the origin being at the center of an LED
+%% The light source is assumed to be an LED matrix centered at origin - the origin being at the center of an LED. We will always illuminate with an odd-numbered square of LEDs, with one always at the center
+N = 2*illumination_layers - 1;   % side of square of illumination matrix
 
+for i = 1:N
+    for j = 1:N
+        % illumination is done in layers
+        % we will calculate the x,y coordinates of the current LED
+        x = (i - illumination_layers)*LED_spacing;
+        y = (j - illumination_layers)*LED_spacing;
+        
+        % Then we convert that into a wave vector
+        wavevector_denominator = sqrt(x^2 + y^2 + illumination_distance^2);
+        kx = wave_number*x/wavevector_denominator;
+        ky = wave_number*y/wavevector_denominator;
+        
+        % generate the illumination matrix
+        illumination_matrix = object_x*kx + object_y*ky;
+        % imshow(illumination_matrix,[])
+        
+        % Then we apply the plane wave to the object
+        % given by exp(jk.r)*obj
+        illuminated_object = illumination_matrix.*object;
+        imshow(abs(illuminated_object),[])
+    end
+end
--- system_constants.m
+++ system_constants.m
@@ -4,6 +4,8 @@
 initial_px = 0.275;     % initial pixel size in microns
 intensity_image = double(imread('C:\Program Files\MATLAB\R2014a\toolbox\images\imdata\cameraman.tif'));
 phase_image = double(imcrop(imread('C:\Program Files\MATLAB\R2014a\toolbox\images\imdata\westconcordorthophoto.png'), [0 0 256 256]));
+object_length = 0.275*size(intensity_image, 1);     % length (and width) in microns
+[object_x, object_y] = meshgrid(-object_length/2:0.275:object_length/2 - 0.275, -object_length/2:0.275:object_length/2 - 0.275);
 
 % Imaging system constants
 sampled_px = 5.5;
@@ -11,4 +13,7 @@ NA = 0.08;              % Numerical aperture of simulated imaging system
 
 % Illumination system constants
 wavelength = 0.632;     % wavelength of light used for simulated illumination, in microns
-LED_distance = 4000;    % vertical and horizontal distance between center of each LED in the array, in microns
+wave_number = 2*pi/wavelength;
+LED_spacing = 4000;    % vertical and horizontal distance between center of each LED in the array, in microns
+illumination_distance = 80000;  % distance from the LED matrix to the object, in microns
+illumination_layers = 3; % no of layers of a spiral square matrix of LEDs used to illuminate the setup
