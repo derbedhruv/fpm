03db99dfe833726136c6f58b44ac6be3aa19dab7
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-05-10 13:46:40 +0530

    succesfully able to reconstruct intensity and phase image from the complex object after manipulation

--- generate_fpm.m
+++ generate_fpm.m
@@ -1,5 +1,9 @@
 %% We're going to generate obliquely illuminated images of an object and sample them at 20 times loss of resolution
-% 
+%% Images chosen are cameraman.tif for intensity and westconcordorthophoto.png from imdata
 
-load(system_constants);
+% First we load constants
+system_constants;
 
+% We generate the simulated object
+phase_image = (pi/256.0).*phase_image;   % normalize phase image to [0, pi] to prevent loss of information in euler's decomposition of exponent
+object = intensity_image.*exp(j*phase_image);
--- system_constants.m
+++ system_constants.m
@@ -3,4 +3,5 @@
 initial_px = 0.275;     % initial pixel size in microns
 sampled_px = 5.5;
 wavelength = 0.632;     % wavelength of light used for simulated illumination, in microns
-
+intensity_image = double(imread('C:\Program Files\MATLAB\R2014a\toolbox\images\imdata\cameraman.tif'));
+phase_image = double(imcrop(imread('C:\Program Files\MATLAB\R2014a\toolbox\images\imdata\westconcordorthophoto.png'), [0 0 256 256]));
