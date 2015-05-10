b49256eca623ca880d0669e2b9df0766c9a293a6
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-05-10 16:14:18 +0530

    commented spiral.m

--- spiral.m
+++ spiral.m
@@ -1,8 +1,9 @@
 function y = spiral(n)      % specify the no of layers in the spiral.
 
-    N = 2*n-1;  % this is the length of each side in the matrix so formed
+    N = 2*n-1;  % this is the length of each side in the square matrix so formed
     mat = zeros(N, N);
 
+    % we're going to generate this spiral from the outside in
     for j = 1:n
         N = 2*(j)-1;
         for i = 0:(N-1)        
